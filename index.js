eb9078aa42edb18cb131ebb28420ef35adf8463e
Jawwaad Sabree <jawwaad.sabree@showseeker.com>
2022-06-11 20:37:13 -0400

    fixed get all rooms

--- index.js
+++ index.js
@@ -89,10 +89,22 @@ const io = require('socket.io')(server, {
   },
 });
 
-let allRooms = {};
+function getActiveRooms(io) {
+  // Convert map into 2D list:
+  // ==> [['4ziBKG9XFS06NdtVAAAH', Set(1)], ['room1', Set(2)], ...]
+  const arr = Array.from(io.sockets.adapter.rooms);
+  // Filter rooms whose name exist in set:
+  // ==> [['room1', Set(2)], ['room2', Set(2)]]
+  const filtered = arr.filter((room) => !room[1].has(room[0]));
+  // Return only the room name:
+  // ==> ['room1', 'room2']
+  const res = filtered.map((i) => i[0]);
+  return res;
+}
 
 io.on('connection', (socket) => {
-  io.emit('allRooms', allRooms);
+  io.emit('allRooms', getActiveRooms(io));
+
   socket.on('correctAnswer', (room) => {
     io.in(room).emit('showCorrectAnswer', socket.id);
   });
@@ -100,10 +112,12 @@ io.on('connection', (socket) => {
     io.emit('nextSong');
   });
   socket.on('joinRoom', (room) => {
-    if (allRooms[room]) allRooms[room].push(socket.id);
-    else allRooms[room] = [socket.id];
-    io.emit('allRooms', allRooms);
     socket.join(room);
+    io.emit('allRooms', getActiveRooms(io));
+  });
+  socket.on('leaveRoom', (room) => {
+    socket.leave(room);
+    io.emit('allRooms', getActiveRooms(io));
   });
   socket.on('getSongs', (songs, artist, room) => {
     const players = [...io.sockets.adapter.rooms.get(room)];
@@ -111,6 +125,6 @@ io.on('connection', (socket) => {
   });
 
   io.on('disconnect', () => {
-    allRooms = {};
+    console.log('disconnected');
   });
 });
